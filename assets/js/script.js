faa652d04e4bffc83865eb9ff01ed2cb83b1be5e
Juan Ramirez <ramirezgutierrez32@gmail.com>
2025-02-14 16:58:58 -0500

    Limpieza de código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 
-
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
 
 
 // sidebar variables
@@ -13,9 +11,8 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
-
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// lenguajes variables
+const lenguajesItem = document.querySelectorAll("[data-lenguajes-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -26,30 +23,30 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const lenguajesModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+for (let i = 0; i < lenguajesItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
+  lenguajesItem[i].addEventListener("click", function () {
 
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalImg.src = this.querySelector("[data-lenguajes-avatar]").src;
+    modalImg.alt = this.querySelector("[data-lenguajes-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector("[data-lenguajes-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-lenguajes-text]").innerHTML;
 
-    testimonialsModalFunc();
+    lenguajesModalFunc();
 
   });
 
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+modalCloseBtn.addEventListener("click", lenguajesModalFunc);
+overlay.addEventListener("click", lenguajesModalFunc);
 
 
 
